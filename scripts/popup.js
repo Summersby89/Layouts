b006500a48de0329499d734f005b44bdf732d8ea
Summersby <pashkov_se@mail.ru>
2020-07-11 14:01:34 +0300

    update first-section, media request, form sript

--- scripts/popup.js
+++ scripts/popup.js
@@ -9,12 +9,7 @@ let unlock = true;
 
 if (popupLinks.length > 0) {
     popupLinks.forEach((el) => {
-    // for (var index = 0; index < popupLinks.length; index++) {
-        // popupLinks = popupLinks[index];
         el.addEventListener('click', function (e) {
-        // popupLinks[index].addEventListener('click', function (e) {
-            
-            // const popupName = popupLinks.getAttribute('href');
             const curentPopup = document.querySelector(".popup");
             popupOpen(curentPopup);
             e.preventDefault();
@@ -25,8 +20,6 @@ let popupCloseIcon = document.querySelectorAll('.popup-close');
     
 if (popupCloseIcon.length > 0) {
     popupCloseIcon.forEach((item) => {
-    // for (let index=0; index < popupCloseIcon.length; index++) {
-        // const el = popupCloseIcon[index];
         item.addEventListener('click', function (e) {
             popupClose(item.closest('.popup'));
             e.preventDefault();
@@ -60,33 +53,6 @@ function popupClose(popupActive, doUnlock = true) {
     }
 }
 // блокируем скролл
-// function bodyLock() {
-//     // считаем ширину скролла
-//     let lockPaddingValue = window.innerWidth - document.body.offsetWidth + 'px';
-   
-    
-//     if (lockPaddingValue.length > 0) {
-//         // for (let index = 0; index < lockPadding.length; index++) {
-//             // const el = lockPadding[index];
-//             lockPaddingValue.forEach((item) => {
-//             item.style.marginRight = lockPaddingValue;
-            
-//         });
-    
-    
-//     body.style.marginRight = lockPaddingValue;
-    
-//     body.classList.add('lock');
-//     fixBlocks.forEach((el) => {
-//         el.style.marginRight = lockPaddingValue;
-//     });
-
-//     unlock = false;
-//     setTimeout(function () {
-//         unlock = true;
-//     },  timeout);
-// }
-// }
 let bodyLock = function () {
     let paddingOffset = window.innerWidth - document.body.offsetWidth + 'px';
       let pagePosition = window.scrollY;
@@ -99,27 +65,7 @@ let bodyLock = function () {
       body.style.top = -pagePosition + 'px';
   }
   
-// function bodyUnlock() {
-//     setTimeout(function () {
-//         if (lockPadding.length > 0) { 
-//             for (let index = 0; index < lockPadding.length; index++) {
-//                 // const el = lockPadding[index];
-//                 lockPadding[index].style.marginRight = '0px';
-//             }
-//         }
-//         body.style.marginRight = '0px';
-//         body.classList.remove('lock');
-//         fixBlocks.forEach((el) => {
-//             el.style.marginRight = '0px';
-//         });
-//     }, timeout);
-
-//     unlock = false;
-//     setTimeout(function () {
-//         unlock = true;
-//     },  timeout);
-    
-// }
+// разблокируем скролл
 let bodyUnlock = function () {
 	let pagePosition = parseInt(document.body.dataset.position, 10);
 	body.style.top = 'auto';
