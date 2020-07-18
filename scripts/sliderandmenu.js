3dad59aeec7cca3618bfc5d66d9c04123c4248da
Summersby <pashkov_se@mail.ru>
2020-07-18 12:11:09 +0300

    update header styles

--- scripts/sliderandmenu.js
+++ scripts/sliderandmenu.js
@@ -2,12 +2,26 @@ $(document).ready(function() {
   $('.header__burger-menu').click(function() {
     $('.header__burger-menu, .header__menu').toggleClass('active');
     $('body').toggleClass('lock');
+  });
 
-  });  
     $(document).on('click', '.header__list-link', function() {
     $('.header__burger-menu, .header__menu').removeClass('active');
     $('body').removeClass('lock');
     }); 
+
+    const header = $('.header');
+	scrollPrev = 0;
+
+$(window).scroll(function() {
+	const scrolled = $(window).scrollTop();
+ 
+	if ( scrolled > 100 && scrolled > scrollPrev ) {
+		header.addClass('out');
+	} else {
+		header.removeClass('out');
+	}
+	scrollPrev = scrolled;
+});
 
     $('.third-section__slider').slick({
         arrows: true,
